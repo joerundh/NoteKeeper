e0f220b3b90b8e6006ed89ea10f0fc4de6e82f5a
joerundh <joerundh.p01@gmail.com>
2025-02-16 23:12:31 +0100

    Changed "localStorage" to "window.localStorage" in NoteManager

--- js/notemanager.js
+++ js/notemanager.js
@@ -1,7 +1,7 @@
 class NoteManager {
     constructor() {
-        if (localStorage.getItem("noteKeeperKeys")) {
-            this.keys = JSON.parse(localStorage.getItem("noteKeeperKeys"));
+        if (window.localStorage.getItem("noteKeeperKeys")) {
+            this.keys = JSON.parse(window.localStorage.getItem("noteKeeperKeys"));
         } else {
             this.keys = [];
             this.storeKeys();
@@ -20,7 +20,7 @@ class NoteManager {
             body: body,
             edited: now
         };
-        localStorage.setItem(`noteKeeper${now}`, JSON.stringify(newNote));
+        window.localStorage.setItem(`noteKeeper${now}`, JSON.stringify(newNote));
         this.keys.unshift(now);
         this.storeKeys();
         
@@ -28,7 +28,7 @@ class NoteManager {
     }
 
     getNote(key) {
-        return JSON.parse(localStorage.getItem(`noteKeeper${key}`));
+        return JSON.parse(window.localStorage.getItem(`noteKeeper${key}`));
     }
 
     getAllNotes() {
@@ -36,7 +36,7 @@ class NoteManager {
     }
 
     editNote(key, title, body) {
-        let note = JSON.parse(localStorage.getItem(`noteKeeper${key}`));
+        let note = this.getNote(key);
         note.title = title;
         note.body = body;
         note.edited = Date.now();
@@ -44,21 +44,21 @@ class NoteManager {
     }
 
     deleteNote(key) {
-        localStorage.removeItem(`noteKeeper${key}`);
+        window.localStorage.removeItem(`noteKeeper${key}`);
         this.keys.splice(this.keys.indexOf(key), 1);
     }
 
     deleteAllNotes() {
-        localStorage.clear();
+        window.localStorage.clear();
         this.keys = [];
         this.storeKeys();
     }
 
     storeNote(note) {
-        localStorage.setItem(`noteKeeper${note.key}`, JSON.stringify(note));
+        window.localStorage.setItem(`noteKeeper${note.key}`, JSON.stringify(note));
     }
 
     storeKeys() {
-        localStorage.setItem("noteKeeperKeys", JSON.stringify(this.keys));
+        window.localStorage.setItem("noteKeeperKeys", JSON.stringify(this.keys));
     }
 }
